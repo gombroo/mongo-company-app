0ebeb93e77b2aba7b3381b83340bef1dea97ddb2
Ola Siem <info@siemieniuk.net>
2020-02-14 12:10:39 +0100

    Get single element from depatments collection, Convert req.params.id to ObjectId

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./../db');
+const ObjectId = require('mongodb').ObjectId;
 
 router.get('/departments', (req, res) => {
   req.db.collection('departments').find().toArray((err, data) => {
@@ -15,6 +16,24 @@ router.get('/departments/random', (req, res) => {
 
 router.get('/departments/:id', (req, res) => {
   res.json(db.departments.find(item => item.id == req.params.id));
+});
+
+router.get('/departments/:id', (req, res) => {
+  req.db.collection('departments').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else if(!data) res.status(404).json({ message: 'Not found' });
+    else res.json(data);
+  });
+});
+
+router.get('/departements/:id', (req, res) => {
+  req.db.collection('deprtements').findOne({ name: 'costam' }), (err, data) => {
+    if(err) {
+      res.status(500).json({ message: err });
+    } else {
+        res.json(data);
+    }
+  };
 });
 
 router.post('/departments', (req, res) => {
