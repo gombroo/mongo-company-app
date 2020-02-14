60e49525784bdd6a5057adc79d21267c9f706726
Ola Siem <info@siemieniuk.net>
2020-02-14 11:00:32 +0100

    Make 'db' available for all endpoints/files, remove CRUD

--- server.js
+++ server.js
@@ -18,62 +18,19 @@ mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUn
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    // make access to 'db' for all endpoints/files, not only for server.js
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
 
     app.use('/api', employeesRoutes);
     app.use('/api', departmentsRoutes);
     app.use('/api', productsRoutes);
 
-    // find
-    db.collection('employees').find(
-      { department: 'IT' }, 
-        (err, data) => {
-          if(!err) {
-            data.each((error, employee) => {
-              console.log(employee);
-            })
-          }
-        });  
-
-    // findOne
-    db.collection('employees').findOne(
-      { department: 'IT' }, 
-        (err, data) => {
-          if(!err) {
-            console.log(data);
-          }
-        });
-
-    // insertOne
-    // db.collection('departments').insertOne(
-    //   { name: 'Management' }, 
-    //     err => {
-    //       if(err) console.log('err');
-    //     });
-
-    // updateOne
-    // db.collection('employees').updateOne(
-    //   { department: 'IT' }, 
-    //   { $set: { salary: 6000 }}
-    // );
-
-    // updateOne + callback
-    db.collection('employees').updateOne(
-      { department: 'IT' }, 
-      { $set: { salary: 6000 }}, 
-        err => {
-          if(err) console.log(err);
-        });
-     
-    // deleteOne
-    db.collection('departments').deleteOne(
-      { name: 'Management' }, 
-        (err) => {
-          if(err) console.log(err);
-        });
-
     app.use((req, res) => {
       res.status(404).send({ message: 'Not found...' });
-    })
+    });
 
     app.listen('8000', () => {
       console.log('Server is running on port: 8000');
