8e1218d1ef525b4ed6ba16cb37ca8aea036a44a0
Ola Siem <info@siemieniuk.net>
2020-02-17 12:14:09 +0100

    Return updated document after put and delete in departments routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -66,7 +66,8 @@ router.put('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
       await Department.updateOne({ _id: req.params.id }, { $set: { name: name }});
-      res.json({ message: 'OK' });
+      // res.json({ message: 'OK' });    
+      res.json(await Department.find());
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -97,7 +98,8 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      // res.json({ message: 'OK' });
+      res.json(await Department.find());
     }
     else res.status(404).json({ message: 'Not found...' });
   }
