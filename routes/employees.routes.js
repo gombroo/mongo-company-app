6270c6443cc04e680da547eec035d3e97411e16c
Ola Siem <info@siemieniuk.net>
2020-02-17 13:22:13 +0100

    Fix typos

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -37,8 +37,8 @@ router.get('/employees/:id', async (req, res) => {
 
 router.post('/employees', async (req, res) => {
   try {
-    const { firstName, lastName, empartment } = req.body;
-    const newEmployee = new Employee({ firstName: firstName, lastName: lastName, empartment: empartment });
+    const { firstName, lastName, department } = req.body;
+    const newEmployee = new Employee({ firstName: firstName, lastName: lastName, department: department });
     await newEmployee.save();
     res.json({ message: 'OK' });
   } 
@@ -48,11 +48,11 @@ router.post('/employees', async (req, res) => {
 });
 
 router.put('/employees/:id', async (req, res) => {
-  const { firstName, lastName, empartment } = req.body;
+  const { firstName, lastName, department } = req.body;
   try {
     const emp = await(Employee.findById(req.params.id));
     if(emp) {
-      await Employee.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, empartment: empartment }});
+      await Employee.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, department: department }});
       res.json({ message: 'OK' });
     }
     else res.status(404).json({ message: 'Not found...' });
